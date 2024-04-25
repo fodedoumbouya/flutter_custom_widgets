cb682fa6af905d18ff5f83e61d844ab902044485
DOUMBOUYA FODE <47141813+fodedoumbouya@users.noreply.github.com>
2024-04-25 11:40:30 +0200

    Add custom widgets to export

--- lib/flutter_custom_widgets.dart
+++ lib/flutter_custom_widgets.dart
@@ -1,1 +1,9 @@
 library flutter_custom_widgets;
+
+export 'src/widgets/customConditionWidget.dart';
+export 'src/widgets/customContainerWidget.dart';
+export 'src/widgets/customImageFileWidget.dart';
+export 'src/widgets/customImageNetworkWidget.dart';
+export 'src/widgets/customImageSVGWidget.dart';
+export 'src/widgets/customImageWidget.dart';
+export 'src/widgets/customTextWidget.dart';
